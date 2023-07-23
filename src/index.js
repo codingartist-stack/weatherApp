a38b36a4218dd37a322a5664d8e02b47ad4e6abf
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-23 15:59:07 -0600

    console logged the information that I want. I think. I want to see if I can get the icons to show up

--- src/index.js
+++ src/index.js
@@ -10,27 +10,34 @@ document.body.appendChild(check);
 //toggle fahrenheit or celsius
 //change the look of the page based on the data
 
-const location = 'montebello';
+const checkLocation = 'montebello';
 
 async function getCurrentWeather() {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=84aeac0362a74d1d87732615232207&q=${location}`,
+    `https://api.weatherapi.com/v1/current.json?key=84aeac0362a74d1d87732615232207&q=${checkLocation}`,
     { mode: 'cors' }
   );
   const locationData = await response.json();
   console.log(locationData);
-
-  //only get the fahrenheit, celsius, condition code, condition text, condition icon, humidity
 }
 
 async function getForecast() {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=84aeac0362a74d1d87732615232207&q=${location}`,
+    `https://api.weatherapi.com/v1/forecast.json?key=84aeac0362a74d1d87732615232207&q=${checkLocation}`,
     { mode: 'cors' }
   );
 
   const locationForecast = await response.json();
-  console.log(locationForecast);
+
+  console.log(locationForecast.location.name);
+  console.log(locationForecast.location.region);
+  console.log(locationForecast.location.country);
+  console.log(locationForecast.location.localtime);
+
+  console.log(locationForecast.current.condition.text);
+  console.log(locationForecast.current.condition.code);
+  console.log(locationForecast.current.temp_c);
+  console.log(locationForecast.current.temp_f);
 }
 
 getCurrentWeather();
