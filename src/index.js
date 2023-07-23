1f6c64b433b03f0c6831864a6e8c199a71bb8790
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-22 19:13:09 -0600

    set up enviroment

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import _ from 'lodash';
 import './style.css';
+
+const check = document.createElement('h2');
+check.textContent = 'Testing Connection';
+
+document.body.appendChild(check);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -19,6 +28,14 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
       },
     ],
   },
