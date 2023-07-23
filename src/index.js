c8eda748a6b05903997e96b6d5b612362b3294dc
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-23 15:45:35 -0600

    set up two functions to grab the weather I might just use forcast because that has the current weather as well.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,33 @@ const check = document.createElement('h2');
 check.textContent = 'Testing Connection hello';
 
 document.body.appendChild(check);
+
+//search specific location
+//toggle fahrenheit or celsius
+//change the look of the page based on the data
+
+const location = 'montebello';
+
+async function getCurrentWeather() {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=84aeac0362a74d1d87732615232207&q=${location}`,
+    { mode: 'cors' }
+  );
+  const locationData = await response.json();
+  console.log(locationData);
+
+  //only get the fahrenheit, celsius, condition code, condition text, condition icon, humidity
+}
+
+async function getForecast() {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=84aeac0362a74d1d87732615232207&q=${location}`,
+    { mode: 'cors' }
+  );
+
+  const locationForecast = await response.json();
+  console.log(locationForecast);
+}
+
+getCurrentWeather();
+getForecast();
