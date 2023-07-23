3f82e26970d73fb88a074cde47a727e5c62fb67d
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-22 19:18:00 -0600

    got my enviorment set up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import _ from 'lodash';
 import './style.css';
 
 const check = document.createElement('h2');
-check.textContent = 'Testing Connection';
+check.textContent = 'Testing Connection hello';
 
 document.body.appendChild(check);
