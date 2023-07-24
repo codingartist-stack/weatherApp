9fb11ca31044141108ada608213d99cffdcfbf6c
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-24 11:29:47 -0600

    move the getForecast function to it's own file and export and imported it

---
+++ src/getForecast.js
@@ -0,0 +1,64 @@
+export default async function getForecast(checkLocation) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=84aeac0362a74d1d87732615232207&q=${checkLocation}`,
+    { mode: 'cors' }
+  );
+
+  const locationForecast = await response.json();
+
+  const cityName = locationForecast.location.name;
+  const cityRegion = locationForecast.location.region;
+  const cityCountry = locationForecast.location.country;
+  const cityLocalTime = locationForecast.location.localtime;
+  const forecastCondition = locationForecast.current.condition.text;
+  const celsius = locationForecast.current.temp_c;
+  const fahrenheit = locationForecast.current.temp_f;
+
+  console.log(locationForecast.location.localtime);
+
+  const imageContainer = document.createElement('div');
+  const iconImage = document.createElement('img');
+  iconImage.classList.add('icon');
+  imageContainer.appendChild(iconImage);
+  iconImage.src = locationForecast.current.condition.icon;
+
+  const dayContainer = document.createElement('div');
+  dayContainer.classList.add('dayContainer');
+
+  const cityInfoContainer = document.createElement('div');
+  cityInfoContainer.classList.add('cityInfoContainer');
+
+  const cityNameContainer = document.createElement('h2');
+  cityNameContainer.textContent = cityName;
+
+  const cityRegionContainer = document.createElement('p');
+  cityRegionContainer.textContent = cityRegion + ', ' + cityCountry;
+
+  cityInfoContainer.appendChild(cityNameContainer);
+  cityInfoContainer.appendChild(cityRegionContainer);
+
+  const tempContainer = document.createElement('div');
+  tempContainer.classList.add('tempContainer');
+
+  const fahrenheitContainer = document.createElement('h2');
+  fahrenheitContainer.textContent = fahrenheit + ' F\u00B0';
+  fahrenheitContainer.classList.add('temperature');
+
+  const celsiusContainer = document.createElement('h2');
+  celsiusContainer.textContent = celsius + ' C\u00B0';
+  celsiusContainer.classList.add('temperature');
+  celsiusContainer.classList.add('hidden');
+
+  tempContainer.appendChild(fahrenheitContainer);
+  tempContainer.appendChild(celsiusContainer);
+  tempContainer.appendChild(imageContainer);
+
+  const forecastConditionContainer = document.createElement('p');
+  forecastConditionContainer.textContent = forecastCondition;
+
+  dayContainer.appendChild(cityInfoContainer);
+  dayContainer.appendChild(tempContainer);
+  dayContainer.appendChild(forecastConditionContainer);
+
+  weatherContainer.appendChild(dayContainer);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import getForecast from './getForecast';
 
 //search specific location
 
@@ -9,62 +10,7 @@ const weatherContainer = document.querySelector('#weatherContainer');
 //toggle fahrenheit or celsius
 //change the look of the page based on the data
 
-const checkLocation = 'chicago';
+const checkLocation = 'las vegas';
+getForecast(checkLocation);
 
-async function getForecast() {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=84aeac0362a74d1d87732615232207&q=${checkLocation}`,
-    { mode: 'cors' }
-  );
-
-  const locationForecast = await response.json();
-  console.log(locationForecast);
-
-  const cityName = locationForecast.location.name;
-  const cityRegion = locationForecast.location.region;
-  const cityCountry = locationForecast.location.country;
-  const cityLocalTime = locationForecast.location.localtime;
-  const forecastCondition = locationForecast.current.condition.text;
-  const celsius = 'celsius: ' + locationForecast.current.temp_c;
-  const fahrenheit = locationForecast.current.temp_f;
-
-  console.log(locationForecast.location.name);
-  console.log(locationForecast.location.region);
-  console.log(locationForecast.location.country);
-  console.log(locationForecast.location.localtime);
-
-  console.log(locationForecast.current.condition.text);
-
-  console.log('celsius: ' + locationForecast.current.temp_c);
-  console.log('fahrenheit: ' + locationForecast.current.temp_f);
-
-  const imageContainer = document.createElement('div');
-  const iconImage = document.createElement('img');
-  imageContainer.appendChild(iconImage);
-  iconImage.src = locationForecast.current.condition.icon;
-
-  const dayContainer = document.createElement('div');
-  dayContainer.classList.add('dayContainer');
-
-  const cityNameContainer = document.createElement('h2');
-  cityNameContainer.textContent = cityName;
-
-  const cityRegionContainer = document.createElement('p');
-  cityRegionContainer.textContent = cityRegion;
-
-  const fahrenheitContainer = document.createElement('h2');
-  fahrenheitContainer.textContent = fahrenheit;
-
-  const forecastConditionContainer = document.createElement('p');
-  forecastConditionContainer.textContent = forecastCondition;
-
-  dayContainer.appendChild(cityNameContainer);
-  dayContainer.appendChild(cityRegionContainer);
-  dayContainer.appendChild(imageContainer);
-  dayContainer.appendChild(fahrenheitContainer);
-  dayContainer.appendChild(forecastConditionContainer);
-
-  weatherContainer.appendChild(dayContainer);
-}
-
-getForecast();
+searchBtn.addEventListener('click', (e) => {});
